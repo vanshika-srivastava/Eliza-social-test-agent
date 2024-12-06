98df3c593fcb086aec45d3e0c292b3f916130760
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2024-12-06 18:52:13 +0400

    setup eliza

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -47,7 +47,7 @@ import readline from "readline";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
 
-import { mainCharacter } from "./src/mainCharacter";
+import { mainCharacter } from "../mainCharacter";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
